8f64efdf476ca2644df4c4e97479b234d6c95773
Jocelyn Q <jocelyn.develop@gmail.com>
2023-10-25 19:33:32 +0200

    [ADD] first 3 versions

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 from discord.ext import commands
 import discord
+import random
 
 intents = discord.Intents.default()
 intents.members = True
@@ -10,16 +11,35 @@ bot = commands.Bot(
     intents = intents # Set up basic permissions
 )
 
-bot.author_id = 0000000  # Change to your discord id
+bot.author_id = 142316744176828416  # Change to your discord id
 
 @bot.event
 async def on_ready():  # When the bot is ready
     print("I'm in")
     print(bot.user)  # Prints the bot's username and identifier
 
+@bot.event
+async def on_message(message):
+    if message.content == "Salut tout le monde":
+        await message.channel.send(f"Salut tout seul {message.author.mention}")
+
+    await bot.process_commands(message)
+
+
 @bot.command()
 async def pong(ctx):
     await ctx.send('pong')
 
-token = "<MY_TOKEN>"
+@bot.command()
+async def name(ctx):
+    user_name = ctx.author.name
+    await ctx.send(user_name)
+
+
+@bot.command()
+async def d6(ctx):
+    await ctx.send(random.randint(1,6))
+
+
+token = ""
 bot.run(token)  # Starts the bot
